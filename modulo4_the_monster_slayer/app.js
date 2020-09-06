93acbf6e69928c4923b81376c26933c659e7f52b
Felipe Ribeiro <felipe.ribeiro@m4u.com.br>
2020-09-06 10:24:34 -0300

    Finalizando o projeto Monster Slayer

--- modulo4_the_monster_slayer/app.js
+++ modulo4_the_monster_slayer/app.js
@@ -13,12 +13,36 @@ new Vue({
             background: 'gray'
         },
         turn: 'player',
+        settings: {
+            player: {
+                attack:{
+                    min: 3,
+                    max: 10
+                },
+                specialAttack: {
+                    min: 10,
+                    max: 20
+                },
+                heal: 10
+            },
+            monster:{
+                attack: {
+                    min: 5,
+                    max: 12
+                },
+                specialAttack: {
+                    min: 5,
+                    max: 12
+                }
+            }
+        },
         actions: []
     },
     methods: {
         startGame: function(){
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.actions = [];
             this.started = true;
         },
         calculateHealthColor: function(val){
@@ -30,47 +54,103 @@ new Vue({
                 return 'red'
             }
         },
-        attack: function(){
-            let damage = Math.floor(Math.random() * 10);
-            this.actions.push(this.turn + ' - Attack (' + damage + ')');
-            this.applyDamage(damage)
+        attack: async function(){
+            this.doAttack();
+            this.changeTurn();
+
+            await sleep(1000);
+
+            // Monster's turn
+            this.doAttack();
             this.changeTurn();
         },
-        specialAttack: function(){
-            let damage = Math.floor(Math.random() * 30);
-            this.actions.push(this.turn + ' - Special Attack(' + damage + ')');
-            this.applyDamage(damage)
+        specialAttack: async function(){
+            this.doSpecialAttack();
+            this.changeTurn();
+
+            await sleep(1000);
+            
+            // Monster's turn
+            this.doSpecialAttack();
             this.changeTurn();
         },
-        heal: function(){
-            this.actions.push(this.turn + ' - Heal')
+        heal: async function(){
+            if(this.playerHealth + this.settings.player.heal > 100){
+                this.playerHealth = 100;
+            }else{
+                this.playerHealth += this.settings.player.heal;
+            }
+
+            this.createLog('heal', this.turn + ' - Heal');
+            this.changeTurn();
+
+            await sleep(1000);
+
+            // Monster's turn
+            this.doAttack();
             this.changeTurn();
         },
         giveUp: function(){
-            this.actions.push(this.turn + ' - Give Up')
-            this.changeTurn();
+            this.createLog('give-up', this.turn + ' - Give Up')
+            this.started = false;
+        },
+        doAttack: function(){
+            let settings = this.settings[this.turn];
+            let damage = this.calculateDamage(settings.attack.min, settings.attack.max);
+            this.createLog('attack', this.turn + ' - Attack (' + damage + ')');
+            this.applyDamage(damage);
+        },
+        doSpecialAttack: function(){
+            let settings = this.settings[this.turn];
+            let damage = this.calculateDamage(settings.specialAttack.min, settings.specialAttack.max);
+            this.createLog('special-attack', this.turn + ' - Special Attack (' + damage + ')');
+            this.applyDamage(damage);
         },
         changeTurn: function(){
             this.turn = this.turn == 'player' ? 'monster' : 'player';
         },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random() * max), min);
+        },
         applyDamage: function(damage){
             if(this.turn == 'player'){
-                this.monsterHealth -= damage; 
+                this.monsterHealth -= damage;
             }else{
                 this.playerHealth -= damage;
             }
+        },
+        createLog: function(action, description){
+            this.actions.unshift({ 
+                'turn': this.turn,
+                'action': action,
+                'description': description
+            });
         }
     },
     watch: {
         playerHealth: function(val){
-            this.playerHealthStyle.width = val + '%';
+            this.playerHealthStyle.width = (val < 0 ? 0 : val)  + '%';
             this.playerHealthStyle.background = this.calculateHealthColor(val);
+            if(val <= 0 && this.started){
+                this.playerHealth = 0;
+                alert('You lost!');
+                this.started = false;
+            }
         },
         monsterHealth: function(val){
-            this.monsterHealthStyle.width = val + '%';
+            this.monsterHealthStyle.width = (val < 0 ? 0 : val) + '%';
             this.monsterHealthStyle.background = this.calculateHealthColor(val);
+            if(val <= 0 && this.started){
+                this.monsterHealth = 0;
+                alert('You won!');
+                this.started = false;
+            }
         }
     }
 
 
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
