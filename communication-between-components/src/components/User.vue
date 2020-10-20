193166216dbfb8e2e1a26b62b6016bf0b043d5f0
Felipe Ribeiro <felipe.ribeiro@m4u.com.br>
2020-10-20 13:55:42 -0300

    Comunication between components

---
+++ communication-between-components/src/components/User.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeUserName">Change User Name</button>
+        <p>Name: {{ name }} </p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                    :name="name" 
+                    :resetUserNameFn="resetUserName"
+                    @userNameReseted="name = $event" 
+                ></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return {
+                name: 'Max'
+            }
+        },
+        methods: {
+            changeUserName(){
+                this.name = 'Anna'
+            },
+            resetUserName(){
+                this.name = 'Max'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ communication-between-components/src/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <button @click="resetUserName">Reset from Custom Event</button>
+        <button @click="resetUserNameFn">Reset from Parent Method</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            resetUserNameFn: Function
+        },
+        methods: {
+            resetUserName(){
+                this.name = 'Max'
+                this.$emit('userNameReseted', this.name)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
